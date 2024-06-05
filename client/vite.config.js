f699895c76d27d33753cc2f0bfc7e3960a7250f7
Christopher <christopher.ferraro34@gmail.com>
2024-06-05 02:24:03 -0400

    add dist folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/SLAB",
+  base: "/SLAB/",
   plugins: [react()],
   server: {
     port: 3000,

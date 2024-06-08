95342e6b337f817fa9e55f55bff9d8c882368f67
Christopher <christopher.ferraro34@gmail.com>
2024-06-08 09:00:43 -0400

    adjusted files

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/SLAB/",
+  base: "/SLAB/", // Adjust this to match your GitHub Pages URL
   server: {
     port: 3000,
     open: true,

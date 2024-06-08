6bf96d502d9b21797c44757867c4c26090e0a0bc
Christopher <christopher.ferraro34@gmail.com>
2024-06-08 07:06:47 -0400

    original

---
+++ client/src/components/ThoughtForm/index.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
+const ThoughtForm = () => {
+  const [thoughtText, setThoughtText] = useState('');
+
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addThought, { error }] = useMutation
+  (ADD_THOUGHT, {
+    refetchQueries: [
+      QUERY_THOUGHTS,
+      'getThoughts',
+      QUERY_ME,
+      'me'
+    ]
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addThought({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setThoughtText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'thoughtText' && value.length <= 280) {
+      setThoughtText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div className="landing">
+      <h3>See Whats Goin' On</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Character Count: {characterCount}/280
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="thoughtText"
+                placeholder="Here's a new thought..."
+                value={thoughtText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-dark text-white btn-block py-3" type="submit">
+                Add Thought
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <>
+          <p>
+            Get the surfers take on the best swells, crazy surf stories, and the best ways to start your day.
+          </p>
+          <Link className="btn btn-md btn-dark m-0 signup" to="/signup">
+            Start Reading!
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ThoughtForm;

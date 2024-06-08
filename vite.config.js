73f29fc5ef82e8345ec51d5f42990af3106b6d26
Christopher <christopher.ferraro34@gmail.com>
2024-06-08 08:17:59 -0400

    vite and html moved

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/SLAB/",
   plugins: [react()],
+  base: "/SLAB/",
   server: {
     port: 3000,
     open: true,
